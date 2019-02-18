bb6e15ebe6f3b3329be1eb6c6c98bb5e255d75f3
Hargobind Atwal <ethaipath@gmail.com>
2019-02-18 14:55:05 -0800

    mvp last commit

--- project/searching.py
+++ project/searching.py
@@ -15,16 +15,14 @@ def linear_search(arr, target):
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  print(len(arr))
   if len(arr) == 0:
-    return -1 # array empty
+    return -1 # empty array
   low = 0
   high = len(arr)-1
 
   # TO-DO: add missing code
   while low <= high:
     middle = (high+low)//2
-    print(target, middle)
     if arr[middle] < target:
       # get RHS
       low = middle+1
@@ -33,13 +31,25 @@ def binary_search(arr, target):
       high = middle-1
     else:
       return middle
+
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  middle = (low+high)/2
+  middle = (low+high)//2
 
   if len(arr) == 0:
-    return -1 # array empty
+    return -1 # empty array
+
   # TO-DO: add missing if/else statements, recursive calls
+
+  print(target, middle)
+  if arr[middle] < target:
+    return binary_search_recursive(arr, target, middle+1, high)
+  elif arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle-1)
+  else:
+    return middle
+
+  return -1 # not found
