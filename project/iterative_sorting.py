57d3448b729e04cd73c2f649416b0110e8f4997b
Hargobind Atwal <ethaipath@gmail.com>
2019-02-18 14:33:00 -0800

    implimented insertion sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,24 +1,45 @@
 # Complete the selection_sort() function below in class with your instructor
 def selection_sort( arr ):
     # loop through n-1 elements
+    if len(arr) == 0:
+        return []
+
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
     return arr
 
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+
+    # check for empty array
+    if len(arr) == 0:
+        return []
+
+    items = [arr[0]]
+    unsorted_start = 1
+    for i in range(unsorted_start, len(arr)):
+        temp = arr[i]
+        j = i
+        while j > 0 and arr[j-1] > arr[j]:
+            swap = arr[j-1]
+            arr[j-1] = arr[j]
+            arr[j] = swap
+            j = j-1
+
+
 
     return arr
 
